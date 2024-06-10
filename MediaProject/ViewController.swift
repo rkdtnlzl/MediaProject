64f04b33e230195f3af71eaafaf8f78d6622382e
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-10 17:26:24 +0900

    [Feat] NoStoryBoard Setting

--- MediaProject/ViewController.swift
+++ MediaProject/ViewController.swift
@@ -9,9 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        configureNavigationItem()
+        configureHierarchy()
+        configureUI()
+        configureLayout()
+    }
+    
+    func configureNavigationItem() {
+        
+        let rightBarButton = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: .plain, target: self, action: #selector(rightBarButtonTapped))
+        navigationItem.rightBarButtonItem = rightBarButton
+        
+        let leftBarButton = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(leftBarButtonTapped))
+        navigationItem.leftBarButtonItem = leftBarButton
+    }
+    
+    func configureHierarchy() {
+        
+        view.addSubview(tableView)
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        tableView.backgroundColor = .gray
+    }
+    
+    func configureLayout() {
+        
+        
+    }
+    
+    @objc func rightBarButtonTapped() {
+        
+    }
+    
+    @objc func leftBarButtonTapped() {
+        
     }
 
 
