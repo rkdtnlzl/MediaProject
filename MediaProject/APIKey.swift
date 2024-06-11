85f6434d0ebc2ebeecb39f81c9aa062a585f047c
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-11 16:54:22 +0900

    [Feat] Gitignore 선언

---
+++ MediaProject/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  MediaProject
+//
+//  Created by 강석호 on 6/11/24.
+//
+
+import Foundation
+
+struct APIKey {
+
+    static let mediaKey = "af72282e0011f60e2f29df4c833ecfe7"
+}
