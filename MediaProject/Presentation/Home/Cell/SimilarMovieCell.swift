a941772fb6e310623b9566262905d5a1d8d17f23
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-24 22:46:24 +0900

    [Feat] 비슷한 영화 CollectionViewCell 생성

---
+++ MediaProject/Presentation/Home/Cell/SimilarMovieCell.swift
@@ -0,0 +1,37 @@
+//
+//  SimilarMovieCell.swift
+//  MediaProject
+//
+//  Created by 강석호 on 6/24/24.
+//
+
+import UIKit
+
+class SimilarMovieCell: UICollectionViewCell {
+    static let identifier = "SimilarMovieCell"
+    
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with movie: Movie) {
+        if let posterPath = movie.poster_path {
+            let urlString = "https://image.tmdb.org/t/p/w500\(posterPath)"
+            if let url = URL(string: urlString) {
+                imageView.kf.setImage(with: url)
+            }
+        }
+    }
+}
