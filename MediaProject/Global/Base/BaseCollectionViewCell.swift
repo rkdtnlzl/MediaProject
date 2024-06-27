b81e330c7a5adaf9e5829c7541ffaa77b33d6dff
rkdtnlzl <rkdtnlzl@naver.com>
2024-06-27 23:40:22 +0900

    [Feat] BaseCollectionViewCell 생성

---
+++ MediaProject/Global/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BaseCollectionViewCelll.swift
+//  MediaProject
+//
+//  Created by 강석호 on 6/27/24.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+}
